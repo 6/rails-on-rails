0e917c791a9db9e874c084a42dcd58303f3dda19
Peter Graham <peterghm@gmail.com>
2012-03-24 13:21:52 -0400

    set locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_debug
+  before_filter :set_debug, :set_locale
 
   def set_debug
     @debug = Rails.env.development? || params[:debug].present? ? 'yes' : 'no'
+  end
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    @locale = I18n.locale
   end
 end
