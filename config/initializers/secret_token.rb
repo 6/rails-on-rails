b5c992a058c4791583411852ac59520a33ef94e0
Peter Graham <peterghm@gmail.com>
2012-03-24 01:02:51 -0400

    rails new rails-boilerplate -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsBoilerplate::Application.config.secret_token = 'bae9cdfc426c0ccb36ef191cc664a036c8629f264f79685a8495dd5168a73093411b3f728f706e2cf71ee8c67bc5b1864eb49aa4d333073fccac62fa34df18fa'
