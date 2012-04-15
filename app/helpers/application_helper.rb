0eda2a6db12c12335d4d5f48ab0acf5dee7ba65e
Peter Graham <pg.sners@gmail.com>
2012-04-15 09:16:13 -0400

    make active links helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def link_to_active(body, url, html_options = {})
+    html_options[:class] ||= ""
+    html_options[:class] += "#{' ' if html_options[:class].present?}active" if current_page?(url)
+    link_to body, url, html_options
+  end
 end
