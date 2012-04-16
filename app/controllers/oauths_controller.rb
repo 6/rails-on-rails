1a8b81e2d36e6bf2890e3d6c1a614a3595c10b63
Peter Graham <pg.sners@gmail.com>
2012-04-16 19:25:43 -0400

    oauth is fun

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -8,18 +8,20 @@ class OauthsController < ApplicationController
   def callback
     provider = params[:provider]
     begin
-      if @user = login_from(provider)
-        redirect_to root_path
-      else      
-        @user = create_from(provider)
+      @user = login_from(provider)
+      raise StandardError unless @user.present?
+      redirect_to root_path
+    rescue
+      begin
+        @user = create_from(provider)    
         # NOTE: this is the place to add '@user.activate!' if you are using user_activation submodule
 
         reset_session # protect from session fixation attack
         auto_login(@user)
         redirect_to root_path
+      rescue
+        redirect_to root_path, :alert => "Failed to login from #{provider.titleize}!"
       end
-    rescue
-      redirect_to root_path, :alert => "Failed to login from #{provider.titleize}!"
     end
   end
 end
