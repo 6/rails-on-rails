8602b1da7fe03773acd5829ee0e8fab98e3ba346
Peter Graham <pg.sners@gmail.com>
2012-04-17 17:53:59 -0400

    link_to_active helper now optionally handles parent element

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
-  def link_to_active(body, url, html_options = {})
+  def link_to_active(body, url, html_options = nil, parent = nil)
+    html_options ||= {}
     html_options[:class] ||= ""
     html_options[:class] += "#{' ' if html_options[:class].present?}active" if current_page?(url)
-    link_to body, url, html_options
+    link = link_to body, url, html_options
+    link = content_tag(parent, link, :class => "#{'active' if current_page?(url)}") if parent.present?
+    link
   end
 end
