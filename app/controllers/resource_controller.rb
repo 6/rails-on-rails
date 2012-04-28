93ec7b2482eeb73241db41931d8b9b9a8dadfff8
Peter Graham <pg.sners@gmail.com>
2012-04-28 15:24:52 -0400

    easier param extraction

--- app/controllers/resource_controller.rb
+++ app/controllers/resource_controller.rb
@@ -4,4 +4,14 @@ class ResourceController < ApplicationController
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
+  
+  private
+  
+  def extract_params(key, params_list)
+    new_params = {}
+    params_list.each do |par|
+      new_params[par] = params[key][par]
+    end
+    new_params
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ResourceController
       return redirect_to root_url if user
     end
     flash.alert = @user.errors.full_messages.to_sentence if @user.errors.any?
-    redirect_to signup_path(:email => params[:user][:email], :full_name => params[:user][:full_name])
+    redirect_to signup_path(extract_params(:user, [:email, :full_name]))
   end
   
   def update
