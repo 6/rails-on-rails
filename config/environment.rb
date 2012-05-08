e10339bc694f74303af48beaa748640aef9a452d
Peter Graham <pg.sners@gmail.com>
2012-05-08 09:48:42 -0400

    use .env w/ foreman

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Load heroku vars from local file
-if Rails.env.development?
-  heroku_env = Rails.root.join('config', 'heroku_env.rb')
-  load(heroku_env) if File.exists?(heroku_env)
-end
-
 # Initialize the rails application
 RailsBoilerplate::Application.initialize!
